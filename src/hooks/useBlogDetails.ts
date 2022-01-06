8b6de009077f8325e0c23ee714ea6cebfc248b66
timothy okooboh <okoobohtimothy@gmail.com>
2022-01-06 09:30:03 +0100

    create useBlogDetails hook

---
+++ src/hooks/useBlogDetails.ts
@@ -0,0 +1,48 @@
+const useBlogDetails = () => {
+    const blogs = [
+        {
+            title: "How to maintain atomic design principles when using vendor components",
+            description: "In this article, I shared insights on how to maintain atomic design principles when using one or more vendor components.",
+            tags: ["vue", "atomic design", "vendor components"] as string[],
+            url: "https://dev.to/timothyokooboh/vue-js-how-to-maintain-atomic-design-principles-when-using-vendor-components-2hf0",
+            datePublished: "Apr 21, 2021"
+        },
+        {
+            title: "Vuex best practices",
+            description: "I shared best practices for using Vuex mutations, actions, and custom vuex plugins.",
+            tags: ["vue", "vuex", "state management"] as string[],
+            url: "https://dev.to/timothyokooboh/vuex-best-practices-45dd",
+            datePublished: "Apr 14, 2021"
+        },
+        {
+            title: "Vue 3: Debugging composable functions",
+            description: "I wrote about possible bugs that could arise when using the composition API and how to resolve them.",
+            tags: ["vue", "vue 3", "composition api"] as string[],
+            url: "https://dev.to/timothyokooboh/vue-3-0-debugging-composable-functions-1h3e",
+            datePublished: "March 28, 2021"
+        },
+        {
+            title: "5 CSS principles you probably did not know about",
+            description: "I shared 5 useful CSS properties that would be helpful to every Frontend engineer.",
+            tags: ["css", "styling"] as string[],
+            url: "https://dev.to/timothyokooboh/5-css-properties-you-probably-did-not-know-about-164i",
+            datePublished: "Sep 19, 2020"
+        },
+        {
+            title: "How to become a better Vue.js developer",
+            description: "I wrote about various ways of leveling up as a Vue.js engineer.",
+            tags: ["vue", "watchers"] as string[],
+            url: "https://dev.to/timothyokooboh/how-to-become-a-better-vue-js-developer-l13",
+            datePublished: "July 9, 2020"
+        },
+        {
+            title: "Understanding watchers in Vue.js",
+            description: "I explained what watchers are in Vue.js and how to watch arrays and objects.",
+            tags: ["vue", "watchers"] as string[],
+            url: "https://dev.to/timothyokooboh/understanding-watchers-in-vue-js-mki",
+            datePublished: "June 11, 2020"
+        },
+    ]
+
+    return blogs;
+}
