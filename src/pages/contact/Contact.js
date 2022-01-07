c76bf7e6047b410f703b1c5db26e0508f4974b74
timothy okooboh <okoobohtimothy@gmail.com>
2022-01-07 22:52:24 +0100

    make fields required

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -2,18 +2,18 @@ import { ContactContainer } from "./Contact.styled";
 
 const Contact = () => {
     return ( 
-        <ContactContainer netlify>
+        <ContactContainer name="portfolio-contact" netlify>
             <div>
                 <label htmlFor="name"> Name </label>
-                <input type="text" name="name" id="name" />
+                <input type="text" name="name" id="name" required />
             </div>
             <div>
                 <label htmlFor="email"> Email </label>
-                <input type="text" name="email" id="email" />
+                <input type="text" name="email" id="email"  required/>
             </div>
             <div>
                 <label htmlFor="message"> Message </label>
-                <textarea name="message" id="message"></textarea>
+                <textarea name="message" id="message" required></textarea>
             </div>
             <button type="submit">Send</button>
         </ContactContainer>
