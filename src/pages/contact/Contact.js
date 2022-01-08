8b8c4c35edb52cb6c3652dfd3c2aea35339cd363
timothy okooboh <okoobohtimothy@gmail.com>
2022-01-08 08:04:30 +0100

    submit netlify form

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,44 +1,44 @@
 import { ContactContainer } from "./Contact.styled";
-// import { useState } from "react";
+import { useState } from "react";
 
 const Contact = () => {
-    // const [name, setName] = useState("");
-    // const [email, setEmail] = useState("");
-    // const [message, setMessage] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
     
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    //     let myForm = document.getElementById('contact');
-    //     let formData = new FormData(myForm)
+        let myForm = document.getElementById('contact');
+        let formData = new FormData(myForm)
         
-    //     fetch("/", {
-    //         method: "POST",
-    //         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //         body: new URLSearchParams(formData).toString()
-    //     })
-    //     .then((res) => res.json())
-    //     .then((data) => console.log(data))
-    // }
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString()
+        })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+    }
 
     return ( 
         <ContactContainer>
-            <form action="https://www.timothyokooboh.com" name="contact" method="POST" data-netlify="true" id="contact" >
+            <form onSubmit={handleSubmit} name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" id="contact" >
                 <div>
                     <label for="name"> Name </label>
-                   { /*} <input type="text" name="name" id="name" required value={name} onInput={(e) => setName(e.target.value)} /> */ }
-                    <input type="text" name="name" id="name" required />
+                   <input type="text" name="name" id="name" required value={name} onInput={(e) => setName(e.target.value)} />
                 </div>
                 <div>
                     <label for="email"> Email </label>
-                   { /* <input type="email" name="email" id="email" value={email} onInput={(e) => setEmail(e.target.value)}  required/> */ }
-                   <input type="email" name="email" id="email" required/> 
+                   <input type="email" name="email" id="email" value={email} onInput={(e) => setEmail(e.target.value)}  required/>
+                   
                 </div>
                 <div>
                     <label for="message"> Message </label>
-                   { /* <textarea name="message" id="message" value={message} onInput={(e) => setMessage(e.target.value)} required></textarea> */ }
-                    <textarea name="message" id="message" required></textarea>
+                   <textarea name="message" id="message" value={message} onInput={(e) => setMessage(e.target.value)} required></textarea>
                 </div>
+
+                <input type="hidden" name="form-name" value="contact" /> 
                 <button type="submit">Send</button>
             </form>
     </ContactContainer>
