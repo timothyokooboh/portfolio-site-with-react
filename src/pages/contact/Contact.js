bbc083bdccd2d50439c3f1161ebe7a4d9b2e9c04
timothy okooboh <okoobohtimothy@gmail.com>
2022-01-08 14:02:08 +0100

    submit netlify form

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,10 +1,23 @@
 import { ContactContainer } from "./Contact.styled";
 
 const Contact = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const form = document.getElementById("form");
+        const data = new FormData(form);
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(data).toString()
+        })
+        .then(() => alert("Message sent successfully!"))
+    }
     
     return ( 
         <ContactContainer>
-            <form name="contact" method="POST" data-netlify="true">
+            <form onSubmit={handleSubmit} name="contact" data-netlify="true" id="form">
                 <input type="hidden" name="form-name" value="contact" />
                 <div>
                     <label for="name"> Name </label>
@@ -12,12 +25,12 @@ const Contact = () => {
                 </div>
                 <div>
                     <label for="email"> Email </label>
-                   <input type="email" name="email" id="email" required/>
+                   <input type="email" name="email" id="email" required  />
                    
                 </div>
                 <div>
                     <label for="message"> Message </label>
-                   <textarea name="message" id="message" required></textarea>
+                   <textarea name="message" id="message" required onInput={(e) => e.target.value } ></textarea>
                 </div>
 
                 <button type="submit">Send</button>
