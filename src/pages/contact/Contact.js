66f22a9a84605509ba136790eda66ecf8e025cde
timothy okooboh <okoobohtimothy@gmail.com>
2022-01-08 06:59:19 +0100

    submit netlify form

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,8 +1,8 @@
-import { ContactContainer } from "./Contact.styled";
-import { useState } from "react";
+// import { ContactContainer } from "./Contact.styled";
+// import { useState } from "react";
 
 const Contact = () => {
-    const [name, setName] = useState("");
+    /*const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
     
@@ -19,20 +19,20 @@ const Contact = () => {
         })
         .then((res) => res.json())
         .then((data) => console.log(data))
-    }
+    }*/
 
     return ( 
       
-    <form name="contact" netlify>
-        <p>
-            <label>Name <input type="text" name="name" /></label>
-        </p>
-        <p>
-            <label>Email <input type="email" name="email" /></label>
-        </p>
-        <p>
-            <button type="submit">Send</button>
-        </p>
+        <form name="contact" netlify>
+            <p>
+                <label>Name <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Email <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
         </form>
     );
 }
